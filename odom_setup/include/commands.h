6ea1ff8a6a88f73c26791c157975b89ce1e788f6
Jatin Kohli <jatinkohli>
2021-02-23 17:13:05 -0500

    Start code for goToNearestGoal in commands.cpp

--- odom_setup/include/commands.h
+++ odom_setup/include/commands.h
@@ -1,9 +1,22 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       commands.h                                                */
+/*    Author:       Jatin                                                     */
+/*    Created:      2/23/21                                                   */
+/*    Description:  Header for high level robot commands                      */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
 #ifndef COMMANDS_H_
 #define COMMANDS_H_
 
 #include "Drive.h"
 
-float getDistanceToBall(Drive* drive, BallCoord* ball);
+const float FIELD_LENGTH_IN = 11.7 * 12;
+const float GOAL_RADIUS = 11.29 / 2;
+
+float getDistanceToCoord(Drive* drive, BallCoord* ball);
 void goToNearestBall(int colorID, Drive* drive);
+void goToNearestGoal(Drive* drive);
 
 #endif
--- odom_setup/src/commands.cpp
+++ odom_setup/src/commands.cpp
@@ -1,13 +1,23 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       commands.cpp                                              */
+/*    Author:       Jatin                                                     */
+/*    Created:      2/23/21                                                   */
+/*    Description:  Implementation of high level robot commands               */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
 #include "commands.h"
 #include "math.h"
 
 /*
- * Gets the distance to the ball at the location from BallCoord,
- * assuming the robot is at drive's current pose
+ * Gets the distance to the coordinate, where x and y are measured 
+ * using normal xy plane conventions with the origin at the center of the field.
+ * Assumes the robot is at drive's current pose
  */
-float getDistanceToBall(Drive* drive, BallCoord* ball) {
-  float xDiff = drive->getPose().x - ball->x;
-  float yDiff = drive->getPose().y - ball->y;
+float getDistanceToCoord(Drive* drive, float x, float y) {
+  float xDiff = drive->getPose().x - x;
+  float yDiff = drive->getPose().y - y;
 
   return sqrt(xDiff * xDiff + yDiff * yDiff);
 }
@@ -17,27 +27,52 @@ float getDistanceToBall(Drive* drive, BallCoord* ball) {
  * 0 corresponds to a red ball and 1 corresponds to a blue ball.
  */
 void goToNearestBall(int desiredColor, Drive* drive) {
-  //TODO make drive object global and remove parameter from above
-  int id = -1;
+  // TODO make drive object global and remove parameter from above
+  BallCoord* nearestBall;
   float minDistance = -1;
 
   for (int i = 0; i < map->getNumBalls(); i++) {
     BallCoord* ball = &map->getBallCoords()[i];
-    float distance = getDistanceToBall(drive, ball);
+    float distance = getDistanceToCoord(drive, ball->x, ball->y);
 
     if (ball->colorID == desiredColor) {
       if (minDistance == -1 || minDistance > distance) {
-        id = i;
+        nearestBall = ball;
         minDistance = distance;
       }
     }
   }
 
-  if (id != -1) {
+  if (minDistance != -1) {
       drive->goTo({
-        map->getBallCoords()[id].x,
-        map->getBallCoords()[id].y,
+        nearestBall->x,
+        nearestBall->y,
         map->getManagerCoords().deg
     });
   }
+}
+
+void goToNearestGoal(Drive* drive) {
+  // TODO account for obstructions
+  float minX, minY, minDistance = -1;
+
+  for (int xGoal = 0; xGoal < 3; xGoal++) { // lower numbers are to the left
+    for (int yGoal = 0; yGoal < 3; yGoal++) { // lower numbers are towards the top
+      float xCenter = (xGoal - 1) * (FIELD_LENGTH_IN) + (1 - xGoal) * GOAL_RADIUS;
+      float yCenter = (yGoal - 1) * (FIELD_LENGTH_IN) + (1 - yGoal) * GOAL_RADIUS;
+      float distance = getDistanceToCoord(drive, xCenter, yCenter);
+
+      if (minDistance == -1 || minDistance > distance) {
+        minX = xCenter;
+        minY = yCenter;
+        minDistance = distance;
+      }
+    }
+  }
+
+  drive->goTo({
+    minX,
+    minY,
+    map->getManagerCoords().deg // TODO fix angle
+  });
 }
